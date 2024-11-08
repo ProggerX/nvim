4f18485c18f4b85712e710e88dbe5d649f16100a
ProggerX <x@proggers.ru>
2024-11-08 09:55:30 +0300

    Added typst

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,10 +1,11 @@
 local lsp = require('lspconfig')
 lsp.clangd.setup {}
+lsp.typst_lsp.setup {}
 lsp.rust_analyzer.setup {}
 lsp.pyright.setup {}
 lsp.gopls.setup {}
 lsp.texlab.setup {}
 lsp.nixd.setup {}
 lsp.hls.setup{}
-lsp.arduino_language_server.setup{}
-lsp.zls.setup{}
+lsp.arduino_language_server.setup {}
+lsp.zls.setup {}
