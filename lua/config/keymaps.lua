98f7ac0ff1031b86939accfbc9d35060528c66f1
ProggerX <x@proggers.ru>
2025-05-04 11:11:51 +0300

    Add neotree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,6 +20,7 @@ vim.keymap.set('n', 'tg', '<CMD>Telescope live_grep<CR>', {desc = 'Telescope liv
 vim.keymap.set('n', '<Esc>', '<CMD>noh<CR>')
 vim.keymap.set("n", "<leader>a", "<cmd>AerialNavToggle<CR>")
 vim.keymap.set("n", "<leader>A", "<cmd>AerialToggle!<CR>")
+vim.keymap.set("n", "<leader>o", "<cmd>Neotree toggle right<CR>")
 
 vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
 vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -97,5 +97,20 @@ return {
 		dependencies = { 'rafamadriz/friendly-snippets' },
 		version = '1.*',
 		opts_extend = { "sources.default" }
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		lazy = false,
+		---@module "neo-tree"
+		---@type neotree.Config?
+		opts = {
+			
+		},
 	}
 }
