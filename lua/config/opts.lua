eb339ffc88fdf3e5b282f59b84c0bc8c3feb9dec
ProggerX <x@proggers.ru>
2025-09-27 18:08:40 +0300

    Change conceallevel

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -7,7 +7,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.tabstop = 4
 vim.opt.smartindent = true
-vim.opt.conceallevel = 2;
+vim.opt.conceallevel = 0;
 
 vim.opt.clipboard = 'unnamedplus'
 
