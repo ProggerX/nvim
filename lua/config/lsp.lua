491fc4e104f4058f1bde0a1d86e089b311611f77
ProggerX <x@proggers.ru>
2026-01-08 01:56:52 +0300

    temp: hls workaround: disable hlint

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,7 +4,14 @@ vim.lsp.enable "rust-analyzer"
 vim.lsp.enable "tinymist" 
 vim.lsp.enable "gopls" 
 vim.lsp.enable "nixd" 
-vim.lsp.enable "hls" 
 
+vim.lsp.config['hls'] = {
+	settings = {
+		haskell = {
+			plugin = { hlint = { globalOn = false } }
+		}
+	}
+}
+vim.lsp.enable "hls" 
 
 vim.diagnostic.config{ virtual_lines = { current_line = true } }
