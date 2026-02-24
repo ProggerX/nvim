31436cd548f57bea21e870eaf175373f7384129a
ProggerX <x@proggers.ru>
2026-02-24 20:12:16 +0300

    change default ident setup

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -3,8 +3,8 @@ vim.opt.wrap = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.smartindent = true
 vim.opt.conceallevel = 0;
