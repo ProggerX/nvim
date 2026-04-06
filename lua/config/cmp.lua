0a5f1db5ca250b69ab4906c0ddae915f46fa6a3c
ProggerX <x@proggers.ru>
2026-04-06 15:40:12 +0300

    fix some cmp binds

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -1,25 +1,25 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 
 require('blink.cmp').setup({
-	appearance = {
-		nerd_font_variant = 'mono'
-	},
-	completion = {
-		documentation = { auto_show = true },
-		list = { selection = { preselect = false } },
-	},
-	sources = {
-		default = { 'snippets', 'lsp', 'path', 'buffer' },
-	},
-	snippets = { preset = 'luasnip' },
-	fuzzy = { implementation = "lua" },
-	keymap = {
-		preset = 'none',
-
-		['<C-j>'] = { 'snippet_forward', 'fallback' },
-		['<C-k>'] = { 'snippet_backward', 'fallback' },
-		['<M-CR>'] = { 'accept' },
-		['<S-Tab>'] = { 'select_prev', 'fallback' },
-		['<Tab>'] = { 'select_next', 'fallback' },
-	}
+  appearance = {
+    nerd_font_variant = 'mono'
+  },
+  completion = {
+    documentation = { auto_show = true },
+    list = { selection = { preselect = false } },
+  },
+  sources = {
+    default = { 'snippets', 'lsp', 'path', 'buffer' },
+  },
+  snippets = { preset = 'luasnip' },
+  fuzzy = { implementation = "lua" },
+  keymap = {
+    preset = 'none',
+    ['<C- >'] = { 'show', 'fallback' },
+    ['<C-.>'] = { 'snippet_forward', 'fallback' },
+    ['<C-,>'] = { 'snippet_backward', 'fallback' },
+    ['<M-CR>'] = { 'accept' },
+    ['<S-Tab>'] = { 'select_prev', 'fallback' },
+    ['<Tab>'] = { 'select_next', 'fallback' },
+  }
 })
